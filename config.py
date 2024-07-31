f9a127bfcbfd7b2a068e52f4a15aa04446b39cce
MYD773 <filmclan.2@gmail.com>
2024-07-31 09:47:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6149132410:AAEDjj3CvBzZj67nqZE-3_ZqL2rEoN_ohjs")
+API_ID = int(os.environ.get("API_ID", "25065882"))
+API_HASH = os.environ.get("API_HASH", "7711af532d45686e38c6b360161e2483")
+OWNER = int(os.environ.get("OWNER", "6302537270"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "OwnerFcBot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "FilmClan")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001920843740"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://jarramahesh24:VLAsRFbaFT8WFbJn@cluster0.gleasay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "jarramahesh24")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQF-eZoAwVOYRsjBIebBJ_tmTTAVQMztJaz54QQd8iPDOtQDWKQ4ip-2vrm5Z6n9v0EGEJnnYc4FdcVGgk6rVLJMWUtxx1F-kNanJCqzlJ8CgHNZa-iOHYYCuy6tk0MeTYRkK4MrsctEDkwGSoZKDUbUqDGGCAfgcd-hgfg9PJ84PMzBOwP8nFl8BO6bEYsOz47B6EaxOmzZ8wRXN0I0LNCIUhA8vm52N2uOVM4Sh48yiX1-l8RfdSJN_UaZjBLm3zVUpgCW9IqfoUzPWlGuZbSkkguvTV3LqzwhSxaujgo1JiT9K2iae3lzSwEl2IuAi0tVNNLd7-DTEUEAGxa5Tzcgn9lyQQAAAAF3qRY2AA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
 
 
